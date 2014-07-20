4bcb7afb9893b6cc8917213cd8756fa81bdfed95
icymoon <1652908000@qq.com>
2014-07-20 14:48:33 +0800

    CREATE
    CREATE TEST

---
+++ test/src/com/caculate.java
@@ -0,0 +1,16 @@
+package com;
+
+public class caculate {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println(1/2);
+		System.out.println(2/2);
+		System.out.println(3/2);
+		System.out.println(4/2);
+	}
+
+}
